a74c8819528d7b5b2b8368e7d97e6b6948a67856
Zyttal <calz483k@gmail.com>
2024-07-05 02:52:51 +0800

    Add Firebase Dependency - API Keys are held in a .env file stored locally and not included in the repo - kotlin.android plugin was raised to 1.8.22 to mitigate duplicate issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:jotter_mapper/firebase_options.dart';
 import 'package:jotter_mapper/routing/router.dart';
 import 'package:jotter_mapper/themes/theme.dart';
 
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   GlobalRouter.initialize();
   runApp(const MainApp());
 }
